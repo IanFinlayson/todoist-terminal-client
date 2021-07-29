53f5c05829b4c3400ff94edf3b4554772f02f60f
Ian Finlayson <finlaysoni@gmail.com>
2021-07-29 10:49:08 -0400

    Initial commit of the program

---
+++ interface.rkt
@@ -0,0 +1,10 @@
+; interface.rkt
+; provides a command-line interface for users to
+; interact with their projects and tasks
+
+(require racket/include)
+(include "rest.rkt")
+
+(get-projects "123")
+
+
---
+++ rest.rkt
@@ -0,0 +1,7 @@
+; rest.rkt
+; functions to interact with the Todoist REST API
+
+(define (get-projects token)
+ (printf "TODO~n"))
+
+
