777901f66686bf1f4568d7715c44652cf8ea2657
Ian Finlayson <finlaysoni@gmail.com>
2021-08-09 20:33:06 -0400

    Start working on config file management

--- src/todoist_terminal_client/config.clj
+++ src/todoist_terminal_client/config.clj
@@ -1,6 +1,31 @@
 (ns todoist-terminal-client.config)
 
-(defn get-token
+(defn config-file-name
+  "Returns the path to the app's config file in the user's home directory"
   []
-  "abcdef")
+  (str (System/getProperty "user.home") "/.config/todo-config.clj"))
+
+; the default settings written to the file
+(def default-settings
+  {:select-color "blue"
+   :sort "normal"
+  })
+
+(defn dump-default-settings
+  "Writes the default settings (given token and default list) to file"
+  [token default-list]
+  (let [full-default-settings
+        (conj {:token token :default-list default-list} default-settings)]
+    (with-open [outfile (clojure.java.io/writer (config-file-name))]
+      (doseq [setting full-default-settings]
+        (.write outfile (str setting "\n"))))))
+
+
+
+
+    
+    
+    ;(spit (config-file-name) full-default-settings)))
+  
+
 
